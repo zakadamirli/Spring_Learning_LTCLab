eab104a86c6b97eb262c366bd1fb0a5b10507ee4
zakadamirli <z3kkadamirli@gmail.com>
2024-11-14 18:41:18 +0400

    test

---
+++ src/test/java/com/zekademirli/laresmain/LaresMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zekademirli.laresmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaresMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
