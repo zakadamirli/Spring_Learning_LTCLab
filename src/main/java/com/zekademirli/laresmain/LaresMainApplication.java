2296d55d5d464f4654336b299459f922605dbcdb
zakadamirli <z3kkadamirli@gmail.com>
2024-11-14 18:37:13 +0400

    main

---
+++ src/main/java/com/zekademirli/laresmain/LaresMainApplication.java
@@ -0,0 +1,12 @@
+package com.zekademirli.laresmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaresMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaresMainApplication.class, args);
+    }
+}
